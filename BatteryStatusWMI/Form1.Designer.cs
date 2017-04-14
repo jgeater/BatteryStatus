a3f8a952a38ae9e929975e82e434cdcb3df43bb0
James Geater <jgeater@outlook.com>
2017-04-14 12:03:08 -0400

    initial commit

---
+++ BatteryStatusWMI/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace BatteryStatus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblBatteryName = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 63);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Current Battery Level :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 17);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(559, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "The Battery must be changed to at least XX% to continue";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 206);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(546, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Charging Can take a long time if the battery is very low. ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 258);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(820, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "The task sequence will continue automatically when the battery reaches 60% charge" +
+    "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(24, 148);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 26);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
+            // 
+            // lblBatteryName
+            // 
+            this.lblBatteryName.AutoSize = true;
+            this.lblBatteryName.Location = new System.Drawing.Point(624, 63);
+            this.lblBatteryName.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblBatteryName.Name = "lblBatteryName";
+            this.lblBatteryName.Size = new System.Drawing.Size(70, 25);
+            this.lblBatteryName.TabIndex = 1;
+            this.lblBatteryName.Text = "label2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(264, 63);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 25);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(470, 63);
+            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(154, 25);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Battery Name: ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(24, 123);
+            this.label8.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(249, 25);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Current Charging Status:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(912, 329);
+            this.ControlBox = false;
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblBatteryName);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Waiting for sufficient battery charge";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblBatteryName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ BatteryStatusWMI/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Management;
+
+namespace BatteryStatus
+{
+    public partial class Form1 : Form
+    {
+        int MinBatLev = 60;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        Dictionary<UInt16, string> StatusCodes;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            StatusCodes = new Dictionary<ushort, string>();
+            StatusCodes.Add(1, "The battery is discharging. Please Plug in the Charger!!");
+            StatusCodes.Add(2, "The system has access to AC. So the battery is Charging.");
+            StatusCodes.Add(3, "Fully Charged");
+            StatusCodes.Add(4, "Low");
+            StatusCodes.Add(5, "Critical");
+            StatusCodes.Add(6, "The battery is Charging");
+            StatusCodes.Add(7, "Charging and High");
+            StatusCodes.Add(8, "Charging and Low");
+            StatusCodes.Add(9, "Undefined");
+            StatusCodes.Add(10,"Partially Charged");
+
+            PowerStatus p = SystemInformation.PowerStatus;
+            int a = (int)(p.BatteryLifePercent * 100);
+
+            timer1.Enabled = true;
+
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("select * from Win32_Battery");
+            foreach (ManagementObject mo in mos.Get())
+            {
+                lblBatteryName.Text = a.ToString()+"%";
+                lblBatteryName.Text = mo["Name"].ToString();
+                label6.Text = a + "%" ;
+                UInt16 statuscode = (UInt16)mo["BatteryStatus"];
+                string statusString = StatusCodes[statuscode];
+                label5.Text = statusString;
+                label2.Text = "The Battery must be changed to at least " + MinBatLev +"% to continue";
+                label4.Text = "The task sequence will continue automatically when the battery reaches " + MinBatLev +"% charge.";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            PowerStatus p = SystemInformation.PowerStatus;
+            int a = (int)(p.BatteryLifePercent * 100);
+
+            if (a > MinBatLev)
+            {
+                Environment.Exit(0);
+            }
+
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("select * from Win32_Battery where Name='"+lblBatteryName.Text+"'");
+            foreach (ManagementObject mo in mos.Get())
+            {
+
+                lblBatteryName.Text = a.ToString() + "%";
+                lblBatteryName.Text = mo["Name"].ToString();
+                label6.Text = a + "%";
+                UInt16 statuscode = (UInt16)mo["BatteryStatus"];
+                string statusString = StatusCodes[statuscode];
+                label5.Text = statusString;
+
+            }
+
+        }
+
+    }
+}
