3adb04ec8a0b0311ff9a4004329b2beae3642c18
James Geater <jgeater@outlook.com>
2017-08-10 09:17:04 -0400

    Added a check for AC adapter

--- BatteryStatusWMI/Form1.Designer.cs
+++ BatteryStatusWMI/Form1.Designer.cs
@@ -49,40 +49,36 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 63);
-            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Location = new System.Drawing.Point(12, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(227, 25);
+            this.label1.Size = new System.Drawing.Size(112, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Current Battery Level :";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(24, 17);
-            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Location = new System.Drawing.Point(12, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(559, 25);
+            this.label2.Size = new System.Drawing.Size(277, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "The Battery must be changed to at least XX% to continue";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(24, 206);
-            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Location = new System.Drawing.Point(12, 107);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(546, 25);
+            this.label3.Size = new System.Drawing.Size(268, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "Charging Can take a long time if the battery is very low. ";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(24, 258);
-            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Location = new System.Drawing.Point(12, 134);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(820, 25);
+            this.label4.Size = new System.Drawing.Size(406, 13);
             this.label4.TabIndex = 6;
             this.label4.Text = "The task sequence will continue automatically when the battery reaches 60% charge" +
     "";
@@ -91,59 +87,54 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(24, 148);
-            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Location = new System.Drawing.Point(12, 77);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(76, 26);
+            this.label5.Size = new System.Drawing.Size(41, 13);
             this.label5.TabIndex = 7;
             this.label5.Text = "label5";
             // 
             // lblBatteryName
             // 
             this.lblBatteryName.AutoSize = true;
-            this.lblBatteryName.Location = new System.Drawing.Point(624, 63);
-            this.lblBatteryName.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblBatteryName.Location = new System.Drawing.Point(312, 33);
             this.lblBatteryName.Name = "lblBatteryName";
-            this.lblBatteryName.Size = new System.Drawing.Size(70, 25);
+            this.lblBatteryName.Size = new System.Drawing.Size(35, 13);
             this.lblBatteryName.TabIndex = 1;
             this.lblBatteryName.Text = "label2";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(264, 63);
-            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Location = new System.Drawing.Point(132, 33);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(70, 25);
+            this.label6.Size = new System.Drawing.Size(35, 13);
             this.label6.TabIndex = 8;
             this.label6.Text = "label6";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(470, 63);
-            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label7.Location = new System.Drawing.Point(235, 33);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(154, 25);
+            this.label7.Size = new System.Drawing.Size(77, 13);
             this.label7.TabIndex = 9;
             this.label7.Text = "Battery Name: ";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(24, 123);
-            this.label8.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label8.Location = new System.Drawing.Point(12, 64);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(249, 25);
+            this.label8.Size = new System.Drawing.Size(122, 13);
             this.label8.TabIndex = 10;
             this.label8.Text = "Current Charging Status:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(912, 329);
+            this.ClientSize = new System.Drawing.Size(456, 171);
             this.ControlBox = false;
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
@@ -155,7 +146,6 @@
             this.Controls.Add(this.lblBatteryName);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Waiting for sufficient battery charge";
--- BatteryStatusWMI/Form1.cs
+++ BatteryStatusWMI/Form1.cs
@@ -11,17 +11,34 @@ namespace BatteryStatus
 {
     public partial class Form1 : Form
     {
+        //Minimum battery level to continue
         int MinBatLev = 60;
         
         public Form1()
         {
             InitializeComponent();
+            //Look foir an AC Adapter and prompt to plug it in before we do anything else
+            PowerStatus powerStatus = SystemInformation.PowerStatus;
+
+            if (powerStatus.PowerLineStatus == PowerLineStatus.Online)
+            {
+                //MessageBox.Show("Running On Power", Convert.ToString(powerStatus.BatteryLifePercent * 100) + "%");
+            }
+            else
+            {
+                PowerStatus cpowerStatus = SystemInformation.PowerStatus;
+                while ((cpowerStatus.PowerLineStatus != PowerLineStatus.Online))
+                {
+                    MessageBox.Show("Please Plug in the AC adapter", "NO AC", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.ServiceNotification);
+                }
+            }
+
 
         }
         Dictionary<UInt16, string> StatusCodes;
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            //Define charging states
             StatusCodes = new Dictionary<ushort, string>();
             StatusCodes.Add(1, "The battery is discharging. Please Plug in the Charger!!");
             StatusCodes.Add(2, "The system has access to AC. So the battery is Charging.");
@@ -39,6 +56,7 @@ namespace BatteryStatus
 
             timer1.Enabled = true;
 
+            //Get current state from WMI
             ManagementObjectSearcher mos = new ManagementObjectSearcher("select * from Win32_Battery");
             foreach (ManagementObject mo in mos.Get())
             {
@@ -55,19 +73,20 @@ namespace BatteryStatus
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-
+            //Watch the battery level and loop until it reached the minbatlev
             PowerStatus p = SystemInformation.PowerStatus;
             int a = (int)(p.BatteryLifePercent * 100);
 
             if (a > MinBatLev)
             {
+                //Battery is at the minimul level so we can exit the loop now
                 Environment.Exit(0);
             }
 
             ManagementObjectSearcher mos = new ManagementObjectSearcher("select * from Win32_Battery where Name='"+lblBatteryName.Text+"'");
             foreach (ManagementObject mo in mos.Get())
             {
-
+                //Update the UI with current stats
                 lblBatteryName.Text = a.ToString() + "%";
                 lblBatteryName.Text = mo["Name"].ToString();
                 label6.Text = a + "%";
